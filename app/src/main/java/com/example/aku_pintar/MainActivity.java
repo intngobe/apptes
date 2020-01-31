5ac46b9214e786613f9299cb77aeceadf4ae93ad
armytxt <intngobel@gmail.com>
2020-02-01 05:17:27 +0800

    myapp

---
+++ app/src/main/java/com/example/aku_pintar/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.aku_pintar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
